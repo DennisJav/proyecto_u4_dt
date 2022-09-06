57e5dd98c1330b5eeaf8038c52ec8911e0e7d2f3
DennisJav <denis.javier.tapia.ortiz@gmail.com>
2022-09-05 20:39:49 -0500

    Taller 38 GET POST DELETE PUT

--- src/main/java/com/uce/ec/controller/PersonaController.java
+++ src/main/java/com/uce/ec/controller/PersonaController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uce.ec.modelo.Persona;
@@ -23,7 +27,53 @@ public class PersonaController {
 	public String buscarTodos(Model modelo) {
 		List<Persona> lista = this.personaJpaService.buscarTodos();
 		modelo.addAttribute("personas",lista);
-		return "vistaListaEstudiantes";
+		return "vistaListaPersonas";
 	}
+	
+	@GetMapping("/buscarUno/{idPersona}")
+	public String buscarPersona(@PathVariable("idPersona") Integer id, Model modelo) {
+		
+		Persona per=this.personaJpaService.buscarPersona(id);
+		modelo.addAttribute("persona",per);
+		
+		return "VistaPersona";
+	}
+	
+	@PutMapping("/actualizar/{idPersona}")
+	public String actualizarPersona(@PathVariable("idPersona") Integer id, Persona persona) {
+		
+		persona.setId(id);
+		this.personaJpaService.actualizarPersona(persona);
+		
+		return "redirect:/personas/buscar";
+	}
+	
+	@DeleteMapping("/borrar/{idPersona}")
+	public String borrarPersona(@PathVariable("idPersona") Integer id) {
+		
+		this.personaJpaService.eliminarPersona(id);
+		
+		return "redirect:/personas/buscar";
+	}
+	
+	@PostMapping("/insertar")
+	public String insertarPersona(Persona persona) {
+		
+		this.personaJpaService.crearPersona(persona);
+		
+		return"redirect:/personas/buscar";
+	}
+	
+	//metodos de redireccionamiento a paginas
+	@GetMapping("/nuevaPersona")
+	public String paginaNuevaPersona(Persona persona) {
+		return "vistaNuevaPersona";
+	}
+	
+	
+	
+	
+	
+	
 	
 }
